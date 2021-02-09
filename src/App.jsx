2d0bea58f3d2eb463959437cfca39ff89fabdc99
kodai <63472444+kodai01@users.noreply.github.com>
2021-02-09 12:29:03 +0900

    7/URLに応じたコンポーネントを表示した

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { signInAction } from './reducks/users/action';
+import React from 'react'
+import Router from './router'
 
-function App() {
-  const dispatch = useDispatch()
-  const selector = useSelector((state) => state)
-  console.log(selector.users)
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={() => dispatch(signInAction({userId: "00001", userName:"japan"}))}>SIGN IN</button>
-      </header>
-    </div>
-  );
+    <main>
+      <Router />
+    </main>
+  )
 }
 
-export default App;
+export default App
